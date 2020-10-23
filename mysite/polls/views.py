a4d97db975820c672ca6b993d06461cc1d8c7ab0
Aashish Budania <budania.aashish@gmail.com>
2020-10-23 08:59:28 +0530

    made admin, added url and views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
 
-# Create your views here.
 
+from .models import Question
+
+
+# Create your views here.
 def index(request):
-	return HttpResponse("Hello World!, at index page")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ','.join(q.question_text for q in latest_question_list)
+	return HttpResponse(output)
+
+
+def detail(request, question_id):
+	return HttpResponse("you are looking at % s . " % question_id)
+
+
+def results(request, question_id):
+	return HttpResponse("You are seeing the results for % s . " % question_id)
+
+
+def vote(request, question_id):
+	return HttpResponse("You are voting for question %s ." % question_id) 
